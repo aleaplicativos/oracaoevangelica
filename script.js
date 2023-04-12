73a716cb68765e9709a54fb7526e4f9d96d919e7
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-04-12 13:57:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,206 @@
+var oracoes = [
+	"Senhor Jesus, eu te agradeço pelo teu amor e misericórdia. Ajuda-me a confiar em ti e a seguir os teus caminhos todos os dias." ,
+
+"Pai celestial, eu te peço que me dê a sabedoria e a força para enfrentar os desafios da vida. Ajuda-me a confiar em ti e a nunca perder a esperança." ,
+
+"Espírito Santo, enche-me com a tua presença e guia-me em tudo o que eu fizer. Dá-me a sabedoria para discernir a tua vontade e a coragem para segui-la." ,
+
+"Senhor, eu te peço que me ajude a perdoar aqueles que me ofenderam e a amar aqueles que são difíceis de amar. Ajuda-me a ser uma luz para o mundo e a fazer a diferença onde quer que eu vá." ,
+
+"Deus, eu te agradeço por todas as bênçãos que recebi. Ajuda-me a ser grato e a compartilhar a tua bondade com os outros." ,
+
+"Jesus, eu te peço que cure todas as minhas feridas e me ajude a encontrar a paz interior. Eu confio em ti e coloco minha vida em tuas mãos.", "Deus, eu te agradeço pela tua fidelidade e amor incondicional. Ajuda-me a confiar em ti em todas as circunstâncias e a seguir os teus caminhos." ,
+
+"Senhor, eu te peço que me ajude a ser um instrumento do teu amor e graça. Usa-me para fazer a diferença neste mundo e para glorificar o teu nome." ,
+
+"Espírito Santo, eu te convido a entrar em meu coração e a guiar a minha vida. Enche-me com a tua presença e ajuda-me a ser um discípulo fiel de Cristo." ,
+
+"Senhor Jesus, eu te agradeço por teres morrido na cruz por mim e por teres me dado a vida eterna. Ajuda-me a viver em gratidão e a compartilhar o teu amor com os outros." ,
+
+"Pai celestial, eu te peço que abençoes a minha família e os meus amigos. Protege-os e guarda-os em todos os momentos." ,
+
+"Senhor, eu te peço que me ajude a perdoar a mim mesmo e aos outros. Ajuda-me a seguir em frente e a viver a vida em plenitude.", "Senhor, eu te peço que me ajude a ouvir a tua voz e a seguir a tua vontade. Guia-me em todas as áreas da minha vida e ajuda-me a confiar em ti em todas as circunstâncias." ,
+
+"Deus, eu te agradeço por tua graça e misericórdia. Ajuda-me a ser um canal do teu amor e a mostrar o teu caráter para o mundo." ,
+
+"Senhor Jesus, eu te peço que cure todas as minhas feridas e me ajude a encontrar a paz interior. Eu confio em ti e coloco minha vida em tuas mãos." ,
+
+"Pai celestial, eu te peço que me ajude a crescer em minha fé e a ser mais como Cristo. Ajuda-me a amar os outros como tu os amas e a fazer a tua vontade em todas as coisas." ,
+
+"Senhor, eu te peço que me ajude a viver em humildade e a reconhecer que tudo o que tenho vem de ti. Ajuda-me a ser grato e a compartilhar as tuas bênçãos com os outros." ,
+
+"Espírito Santo, eu te peço que me enchas com a tua presença e me ajude a viver uma vida de santidade. Ajuda-me a renunciar ao pecado e a buscar a tua vontade em todas as coisas.", "Senhor, dá-me força para seguir em frente.",
+
+"Deus, ajuda-me a confiar em ti em todos os momentos.",
+
+"Senhor, eu te agradeço por mais um dia de vida.",
+
+"Deus, eu te peço sabedoria para tomar as decisões certas.",
+
+"Senhor, eu te peço que me dês paz em meio às tempestades da vida.",
+
+"Deus, eu te peço que me ajudes a ser um instrumento do teu amor para aqueles que estão ao meu redor.", "Senhor, eu te peço que me ajude a ser um canal do teu amor e da tua graça para aqueles que estão ao meu redor. Ajuda-me a ser sensível às necessidades dos outros, a compartilhar as tuas bênçãos com os necessitados e a ser um exemplo de vida para aqueles que ainda não te conhecem. Ajuda-me a viver uma vida de santidade e a buscar a tua vontade em todas as coisas. Sei que em ti encontro a paz e a segurança que só tu podes dar. Que o teu nome seja glorificado em minha vida, agora e para sempre. Amém.", "Deus, hoje eu quero te pedir que derrames a tua bênção sobre o meu trabalho e sobre os projetos que tenho em mente. Ajuda-me a ser fiel, dedicado e diligente em tudo o que faço, a buscar a excelência e a fazer a tua vontade em todas as coisas. Ajuda-me a ser um exemplo de vida para os meus colegas e a mostrar a tua graça e o teu amor para aqueles que ainda não te conhecem. Sei que em ti encontro a força e a sabedoria que preciso para enfrentar os desafios do dia a dia. Que o teu nome seja glorificado em meu trabalho, agora e para sempre. Amém.", "Senhor, hoje eu quero te pedir que derrames a tua bênção sobre a minha igreja e sobre todos os seus membros. Ajuda-nos a ser um povo dedicado à tua palavra, comprometido com a tua vontade e amoroso para com os outros. Ajuda-nos a crescer em comunhão, em fé e em amor, a ser um exemplo de vida para aqueles que ainda não te conhecem. Que a tua presença e a tua graça sejam a nossa luz e o nosso guia em todas as coisas. Que a tua igreja seja um lugar de paz, de amor e de esperança para aqueles que buscam a verdade. Que o teu nome seja glorificado em nossa igreja, agora e para sempre. Amém.", "Deus, eu te peço que me ajudes a crescer em fé e em amor a cada dia. Amém.",
+
+"Senhor, eu te peço que me ajudes a perdoar aqueles que me magoaram. Amém.",
+
+"Deus, eu te peço que me ajudes a ser um exemplo de vida para os meus filhos. Amém.",
+
+"Senhor, eu te agradeço por tua fidelidade e por sempre estares presente em minha vida. Amém.",
+
+"Deus, eu te peço que me ajudes a ser humilde e a reconhecer a minha dependência de ti. Amém.",
+
+"Senhor, eu te peço que me ajudes a ser grato pelas tuas bênçãos e a compartilhá-las com os outros. Amém.", "Deus, eu te peço que me ajudes a amar o próximo como a mim mesmo. Amém.",
+
+"Senhor, eu te peço que me ajudes a ser fiel a ti em todas as circunstâncias. Amém.",
+
+"Deus, eu te peço que me dês força para enfrentar os desafios da vida. Amém.",
+
+"Senhor, eu te peço que me ajudes a ter um coração grato mesmo em meio às dificuldades. Amém.",
+
+"Deus, eu te peço que me ajudes a ser uma pessoa mais paciente e tolerante. Amém.",
+
+"Senhor, eu te peço que me ajudes a ser um instrumento do teu amor para aqueles que precisam. Amém.",
+
+"Deus, eu te peço que me ajudes a vencer a inveja e a cobiça. Amém.",
+
+"Senhor, eu te peço que me ajudes a ser um canal de paz e de harmonia em meu lar. Amém.",
+
+"Deus, eu te peço que me ajudes a ser um cristão autêntico e comprometido com a tua causa. Amém.",
+
+"Senhor, eu te agradeço por tua misericórdia e pelo teu perdão que me renova a cada dia. Amém.", "Deus, fortalece a minha fé para que eu possa crer em teus milagres. Amém.",
+
+"Senhor, ajuda-me a confiar em ti mesmo quando as circunstâncias parecem desfavoráveis. Amém.",
+
+"Deus, eu te peço que me ajude a crer em teus planos e propósitos para a minha vida. Amém.",
+
+"Senhor, aumenta a minha fé para que eu possa ver o teu poder em ação em minha vida. Amém.",
+
+"Deus, eu te peço que me ajude a perseverar na fé mesmo quando a jornada parecer difícil. Amém.",
+
+"Senhor, eu te peço que me ajude a crer em tua graça e em teu amor por mim. Amém.",
+
+"Deus, eu te peço que me ajude a confiar em tua sabedoria e em teu cuidado por mim. Amém.",
+
+"Senhor, eu te peço que me ajude a crer na tua palavra e a obedecê-la com fidelidade. Amém.",
+
+"Deus, eu te peço que me ajude a crer que nada é impossível para ti. Amém.",
+
+"Senhor, eu te agradeço pela fé que me dás e pela certeza da vida eterna que tenho em ti. Amém.", "Senhor, eu te peço que restaures a minha saúde e renoves as minhas forças. Amém.",
+
+"Deus, eu te peço que me concedas saúde física, emocional e espiritual. Amém.",
+
+"Senhor, eu te peço que me ajudes a cuidar bem do meu corpo, que é o templo do Espírito Santo. Amém.",
+
+"Deus, eu te peço que me ajudes a lidar com as enfermidades com paciência e confiança em ti. Amém.",
+
+"Senhor, eu te peço que me ajude a buscar tratamentos médicos adequados e a seguir as orientações dos profissionais da saúde. Amém.",
+
+"Deus, eu te peço que me dês sabedoria para tomar decisões saudáveis e para evitar hábitos que prejudicam a minha saúde. Amém.",
+
+"Senhor, eu te peço que me ajude a manter uma vida equilibrada, com alimentação saudável, exercícios físicos e descanso adequado. Amém.",
+
+"Deus, eu te peço que me dês esperança e fé mesmo em meio às enfermidades. Amém.",
+
+"Senhor, eu te agradeço pela saúde que tenho e pela capacidade de cuidar de mim mesmo. Amém.",
+
+"Deus, eu te agradeço pela vida e pela oportunidade de servir-te através do meu corpo são. Amém.", "Deus, eu creio que tu és capaz de fazer milagres em minha vida. Ajuda-me a confiar em ti. Amém.",
+
+"Senhor, eu te peço que faças um milagre em minha vida e que eu possa testemunhar do teu poder. Amém.",
+
+"Deus, eu te peço que faças um milagre em minha família e que ela possa conhecer o teu amor e a tua salvação. Amém.",
+
+"Senhor, eu te peço que faças um milagre em minha cidade e que muitas pessoas venham a conhecer-te através dele. Amém.",
+
+"Deus, eu te peço que faças um milagre em minha nação e que ela se volte para ti em arrependimento e fé. Amém.",
+
+"Senhor, eu te agradeço pelos milagres que já realizaste em minha vida e por aqueles que ainda estás por realizar. Amém.",
+
+"Deus, eu te peço que me dês a coragem de orar por milagres e a fé para crer que tu és capaz de realizá-los. Amém.",
+
+"Senhor, eu te peço que me ajudes a confiar em ti mesmo quando as circunstâncias parecem impossíveis. Amém.",
+
+"Deus, eu te peço que faças um milagre em minha igreja e que ela seja uma luz para o mundo através da tua graça e poder. Amém.",
+
+"Senhor, eu te louvo pelo milagre da minha salvação e pela esperança da vida eterna em ti. Amém.", "Deus, eu reconheço os meus erros e peço o teu perdão. Ajuda-me a caminhar em tua verdade e em teu amor. Amém.",
+
+"Senhor, eu peço perdão por todas as vezes que eu te desobedeci e não segui o teu caminho. Ajuda-me a viver de acordo com a tua vontade. Amém.",
+
+"Deus, eu te peço que me ajudes a perdoar aqueles que me magoaram e me feriram. Ajuda-me a seguir o teu exemplo de amor e graça. Amém.",
+
+"Senhor, eu te peço que me perdoes por todas as vezes que eu julguei e condenei os outros sem misericórdia. Ajuda-me a ser mais compassivo e humilde. Amém.",
+
+"Deus, eu te peço que me ajudes a perdoar a mim mesmo pelos erros do passado e seguir em frente com confiança em ti. Amém.",
+
+"Senhor, eu te peço que me ajude a perdoar aqueles que não me pediram perdão, mas que eu saiba que devo liberar o perdão por amor a ti. Amém.",
+
+"Deus, eu te peço que me ajude a ser um agente de perdão no mundo, espalhando a tua graça e misericórdia por onde eu passar. Amém.",
+
+"Senhor, eu te agradeço por perdoar os meus pecados e por me amar incondicionalmente. Ajuda-me a viver em gratidão e obediência a ti. Amém.",
+
+"Deus, eu te peço que me ajudes a ser mais como Jesus, perdoando sempre e amando incondicionalmente. Amém.",
+
+"Senhor, eu te peço que me ajude a lembrar que o perdão é uma escolha e que eu devo escolher sempre perdoar, assim como tu perdoaste a mim. Amém.", "Deus, ajuda-me a ser paciente em meio às dificuldades e desafios da vida. Que eu confie em ti e saiba que a tua graça é suficiente para mim. Amém.",
+
+"Senhor, eu te peço que me ajude a ser paciente com aqueles que me rodeiam, lembrando que cada pessoa tem a sua própria história e lutas. Amém.",
+
+"Deus, ajuda-me a cultivar a paciência em meu coração, para que eu possa esperar com alegria as tuas bênçãos e planos para minha vida. Amém.",
+
+"Senhor, eu te agradeço pela paciência que tens comigo, mesmo quando eu sou impaciente e ansioso. Ajuda-me a ser mais como ti. Amém.",
+
+"Deus, ajuda-me a ser paciente e perseverante em minha vida de oração e estudo da tua Palavra. Que eu possa aprender mais sobre ti e crescer em fé. Amém.",
+
+"Senhor, eu te peço que me ajude a ser paciente em meio às provações e sofrimentos, sabendo que o teu amor por mim é constante e fiel. Amém.",
+
+"Deus, ajuda-me a ser paciente em relação ao tempo de resposta às minhas orações, lembrando que tu tens o controle de todas as coisas e age no momento certo. Amém.",
+
+"Senhor, eu te peço que me ajude a ser paciente comigo mesmo, lembrando que sou uma obra em progresso e que tu estás trabalhando em mim. Amém.",
+
+"Deus, ajuda-me a ser paciente com aqueles que me ofendem e me magoam, lembrando que o perdão é o caminho para a paz e a cura. Amém.",
+
+"Senhor, eu te peço que me ajude a ser paciente em minha jornada de crescimento espiritual, lembrando que a tua graça é suficiente para mim e que tu estás me capacitando. Amém.", "Deus, concede-me a tua paz que excede todo o entendimento, para que eu possa descansar em ti e não me abalar diante das circunstâncias da vida. Amém.",
+
+"Senhor, eu te peço que me ajude a ser um pacificador em meu lar, na minha igreja e em minha comunidade, levando a paz que vem de ti. Amém.",
+
+"Deus, ajuda-me a cultivar a paz em meu coração, para que eu possa ser um canal da tua paz para aqueles ao meu redor. Amém.",
+
+"Senhor, eu te agradeço pela paz que sinto quando estou em tua presença e peço que me ajude a permanecer em ti para que eu possa viver em paz. Amém.",
+
+"Deus, ajuda-me a perdoar aqueles que me feriram, para que eu possa experimentar a paz que vem do teu perdão. Amém.",
+
+"Senhor, eu te peço que me ajude a confiar em ti em meio aos tempos difíceis, lembrando que tu és a minha rocha e fortaleza. Amém.",
+
+"Deus, ajuda-me a ser um agente de paz em um mundo tão cheio de conflitos e divisões, para que eu possa ser um reflexo do teu amor. Amém.",
+
+"Senhor, eu te peço que me ajude a buscar a paz contigo todos os dias, para que eu possa experimentar a alegria que vem da comunhão contigo. Amém.",
+
+"Deus, ajuda-me a ser paciente em meio às provações e a confiar que tu estás no controle de todas as coisas, para que eu possa ter paz. Amém.",
+
+"Senhor, eu te peço que me ajude a ser grato por tudo o que tenho e por tudo o que sou, para que eu possa viver em paz e com alegria. Amém.", "Senhor, ajuda-me a administrar bem o que tu me deste, para que eu possa ser fiel no pouco e ser abençoado no muito. Amém.",
+
+"Deus, eu te peço que me ajude a ser sábio com o dinheiro que tu me confiaste, para que eu possa honrar-te em todas as áreas da minha vida. Amém.",
+
+"Senhor, eu te peço que me ajude a confiar em ti para suprir todas as minhas necessidades, sabendo que tu és o meu provedor fiel. Amém.",
+
+"Deus, ajuda-me a ser generoso com o que tu me deste, para que eu possa abençoar os outros e ser abençoado por ti em troca. Amém.",
+
+"Senhor, eu te peço que me ajude a ser diligente em meu trabalho, para que eu possa ser recompensado por minhas habilidades e esforços. Amém.",
+
+"Deus, ajuda-me a evitar a tentação de confiar na riqueza deste mundo, mas sim colocar minha confiança em ti e nas tuas promessas. Amém.",
+
+"Senhor, eu te peço que me ajude a ser responsável com as minhas finanças, para que eu possa estar livre das preocupações e viver em paz. Amém.",
+
+"Deus, ajuda-me a ser grato pelo que tu me deste, reconhecendo que tudo o que tenho vem das tuas mãos generosas. Amém.",
+
+"Senhor, eu te peço que me ajude a usar sabiamente as oportunidades de investimento, para que eu possa ser um bom mordomo do que tu me confiaste. Amém.",
+
+"Deus, ajuda-me a ser humilde e não me orgulhar daquilo que tenho, lembrando que tudo vem de ti e que sou apenas um mordomo dos teus recursos. Amém.",
+
+
+
+
+];
+
+var randomIndex = Math.floor(Math.random() * oracoes.length);
+var oracao = oracoes[randomIndex];
+
+document.getElementById("oracao").innerHTML = oracao;
